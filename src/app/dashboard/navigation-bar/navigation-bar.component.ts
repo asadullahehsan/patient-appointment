815cf75bd112934298950b44ea96f0dd78a39ecf
Asadullah Ehsan <asadullahehsan10@gmail.com>
2025-04-27 17:15:57 +0500

    implement navigation bar

--- src/app/dashboard/navigation-bar/navigation-bar.component.ts
+++ src/app/dashboard/navigation-bar/navigation-bar.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navigation-bar',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.css'
+  styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent {
-
+  navItems = [
+    { icon: 'visibility', label: 'Potilaan tarkastelu' },
+    { icon: 'chat', label: 'Potilaan viestit' },
+    { icon: 'calendar_month', label: 'Potilaan kalenteri' },
+    { icon: 'medical_services', label: 'Potilaan palvelut' },
+    { icon: 'group', label: 'Tiimin tarkastelu' },
+    { icon: 'mail', label: 'Tiimin viestit' }
+  ];
 }
