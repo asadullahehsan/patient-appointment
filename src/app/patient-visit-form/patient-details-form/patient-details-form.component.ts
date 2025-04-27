9e8bac153424c119e1abe9d65c5ae7c1754d84c3
Asadullah Ehsan <asadullahehsan10@gmail.com>
2025-04-27 20:23:05 +0500

    add Patient Details Form sub component

--- src/app/patient-visit-form/patient-details-form/patient-details-form.component.ts
+++ src/app/patient-visit-form/patient-details-form/patient-details-form.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-patient-details-form',
-  imports: [],
+  imports: [MatFormFieldModule, FormsModule, MatInputModule],
   templateUrl: './patient-details-form.component.html',
   styleUrl: './patient-details-form.component.css'
 })
 export class PatientDetailsFormComponent {
-
+  patientName = '';
+  birthDate: string | null = null;
+  patientId = '';
 }
--- src/app/patient-visit-form/patient-visit-form.component.ts
+++ src/app/patient-visit-form/patient-visit-form.component.ts
@@ -5,10 +5,11 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { PatientDetailsFormComponent } from "./patient-details-form/patient-details-form.component";
 
 @Component({
   selector: 'app-patient-visit-form',
-  imports: [MatIconModule, DatePipe, MatFormFieldModule, MatRadioModule, FormsModule, MatInputModule],
+  imports: [MatIconModule, DatePipe, MatFormFieldModule, MatRadioModule, FormsModule, MatInputModule, PatientDetailsFormComponent],
   templateUrl: './patient-visit-form.component.html',
   styleUrl: './patient-visit-form.component.css'
 })
