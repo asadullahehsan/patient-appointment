a17014e41f5d31186def2df94657639ec1dc9eb0
Asadullah Ehsan <asadullahehsan10@gmail.com>
2025-04-27 17:27:24 +0500

    implement work list

--- src/app/dashboard/worklist/worklist.component.ts
+++ src/app/dashboard/worklist/worklist.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { WorklistItemComponent } from "./worklist-item/worklist-item.component";
 
 @Component({
   selector: 'app-worklist',
-  imports: [WorklistItemComponent],
+  imports: [WorklistItemComponent, MatIconModule],
   templateUrl: './worklist.component.html',
   styleUrl: './worklist.component.css'
 })
 export class WorklistComponent {
-
+  worklistItems = [
+    { time: '09:00', name: 'Juha Lahtinen', status: 'checked', room: 'Huone 5' },
+    { time: '09:45', name: 'Helena Nieminen', status: 'pending', room: 'Huone 5' },
+    { time: '10:30', name: 'Marko Salminen', status: 'pending', room: 'Huone 5' },
+    { time: '11:15', name: 'Laura Virtanen', status: 'pending', room: 'Huone 5' },
+    { time: '12:00', name: 'Mikko Mäkelä', status: 'pending', room: 'Huone 5' }
+  ];
 }
