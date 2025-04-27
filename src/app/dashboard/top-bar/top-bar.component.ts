95bae23a13757cce1554ac637270939228e5f485
Asadullah Ehsan <asadullahehsan10@gmail.com>
2025-04-27 16:56:15 +0500

    implement top bar

--- src/app/dashboard/top-bar/top-bar.component.ts
+++ src/app/dashboard/top-bar/top-bar.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDividerModule,
+    MatToolbarModule,
+    FormsModule
+  ],
   templateUrl: './top-bar.component.html',
-  styleUrl: './top-bar.component.css'
+  styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
-
+  searchQuery = '';
 }
