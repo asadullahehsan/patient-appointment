860ca4cf1d1afef7babd0060597743bb028d5231
Asadullah Ehsan <asadullahehsan10@gmail.com>
2025-04-27 20:46:57 +0500

    status selector sub component

--- src/app/patient-visit-form/patient-visit-form.component.ts
+++ src/app/patient-visit-form/patient-visit-form.component.ts
@@ -7,16 +7,15 @@ import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { PatientDetailsFormComponent } from "./patient-details-form/patient-details-form.component";
 import { VisitDetailsFormComponent } from "./visit-details-form/visit-details-form.component";
+import { StatusSelectorComponent } from "./status-selector/status-selector.component";
 
 @Component({
   selector: 'app-patient-visit-form',
-  imports: [MatIconModule, DatePipe, MatFormFieldModule, MatRadioModule, FormsModule, MatInputModule, PatientDetailsFormComponent, VisitDetailsFormComponent],
+  imports: [MatIconModule, DatePipe, MatFormFieldModule, MatRadioModule, FormsModule, MatInputModule, PatientDetailsFormComponent, VisitDetailsFormComponent, StatusSelectorComponent],
   templateUrl: './patient-visit-form.component.html',
   styleUrl: './patient-visit-form.component.css'
 })
-export class PatientVisitFormComponent {
-  statuses = ['Luonnos', 'Valmis', 'Hyväksytty'];
-  selectedStatus = '';
+export class PatientVisitFormComponent {  
   doctor = 'Tuomas Veikko Kerola';
   appointmentDate = new Date(2024, 9, 29, 14, 49); // month is 0-indexed
 }
--- src/app/patient-visit-form/status-selector/status-selector.component.ts
+++ src/app/patient-visit-form/status-selector/status-selector.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-status-selector',
-  imports: [],
+  imports: [FormsModule, MatRadioModule],
   templateUrl: './status-selector.component.html',
   styleUrl: './status-selector.component.css'
 })
-export class StatusSelectorComponent {
-
+export class StatusSelectorComponent {  
+  statuses = ['Luonnos', 'Valmis', 'Hyväksytty'];
+  selectedStatus = '';
 }
